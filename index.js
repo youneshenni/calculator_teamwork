9b1e2cd2926dc8fc0a62d69f9b73b28c2d5a78e8
Malik <kbelhadri@gmail.com>
2022-12-06 16:11:27 +0100

    add

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const premier = prompt("ecrit un nombre");
-const plus = prompt("+,-,*,/");
-const desieme = prompt("ecrit un nombre");
-if (plus === "+") {
-  const resulta = premier + desieme;
-  alert(resulta);
+function add() {
+  const premier = prompt("ecrit un nombre");
+  const plus = prompt("+,-,*,/");
+  const desieme = prompt("ecrit un nombre");
+  if (plus === "+") {
+    const resulta = premier + desieme;
+    alert(resulta);
+  }
 }
