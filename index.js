7a7746de05a06ee0f6bd6865b421be782124e785
Zakaria Rahou <zakaria.rahou@inttic.dz>
2022-12-06 15:40:39 +0000

    modulo

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function modulo(a,b){
 
-     alert('le reste de division est'(a%b))
+    try {
+        if(b===0){
+            throw error;
+        }
+        alert('le reste de division est'(a%b)) 
+    } catch (error) {
+        console.log("on peut pas diviser sur 0 !")
+    }
+     
 }
